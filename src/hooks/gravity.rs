f009dbf2664b33f9b4d7ee6dff3096f1b26258b6
ThatNintendoNerd <samtheman1026@gmail.com>
2024-08-27 15:17:40 -0700

    Split `set_gravity_param` function into two functions

--- src/hooks/gravity.rs
+++ src/hooks/gravity.rs
@@ -1,8 +1,6 @@
 use serde::Deserialize;
 use smash_stage::app::StageID;
 
-use crate::config::Config;
-
 /// The parameters for gravity.
 #[derive(Deserialize)]
 pub struct GravityParam {
@@ -28,26 +26,30 @@ struct GravityPos {
 }
 
 /// Updates the parameters for gravity if the given stage identifier is assigned specialized gravity parameters.
-pub fn set_gravity_param(stage_id: StageID) {
+pub fn try_set_gravity_param(stage_id: StageID) {
+    use crate::config::Config;
+
+    if let Some(param) = Config::get().gravity_param.get(&stage_id) {
+        set_gravity_param(param);
+    }
+}
+
+/// Updates the parameters for gravity.
+pub fn set_gravity_param(param: &GravityParam) {
     use smash::app;
 
-    let Some(param) = Config::get().gravity_param.get(&stage_id) else {
-        return;
-    };
-    let Some(instance) = app::BattleObjectWorld::instance_mut() else {
-        return;
-    };
+    if let Some(instance) = app::BattleObjectWorld::instance_mut() {
+        if instance.is_gravity_normal != param.is_gravity_normal {
+            instance.is_gravity_normal = param.is_gravity_normal;
+        }
 
-    if instance.is_gravity_normal != param.is_gravity_normal {
-        instance.is_gravity_normal = param.is_gravity_normal;
-    }
+        if instance.is_gravity_normal {
+            return;
+        }
 
-    if instance.is_gravity_normal {
-        return;
-    }
-
-    if let Some(pos) = &param.pos {
-        instance.gravity_pos.x = pos.x;
-        instance.gravity_pos.y = pos.y;
+        if let Some(pos) = &param.pos {
+            instance.gravity_pos.x = pos.x;
+            instance.gravity_pos.y = pos.y;
+        }
     }
 }
--- src/hooks/stage.rs
+++ src/hooks/stage.rs
@@ -7,7 +7,7 @@ pub fn stage_base_pre_setup(stage_base: &StageBase) {
     original!()(stage_base);
 
     hooks::ground::register_dynamic_collisions(stage_base);
-    hooks::gravity::set_gravity_param(stage_base.stage_id());
+    hooks::gravity::try_set_gravity_param(stage_base.stage_id());
 }
 
 #[skyline::hook(offset = Offsets::get().is_flat_stage)]
