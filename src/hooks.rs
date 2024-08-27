addb3799f645fed7374dfdb57eb2bccce3f66e3f
ThatNintendoNerd <samtheman1026@gmail.com>
2024-08-27 15:52:44 -0700

    Move hook installation to standalone function

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,4 +1,15 @@
-pub(super) mod gravity;
-pub(super) mod ground;
-pub(super) mod settings;
-pub(super) mod stage;
+pub(crate) mod gravity;
+mod ground;
+mod settings;
+mod stage;
+
+/// Installs all the function hooks and memory patches.
+pub fn install() {
+    skyline::install_hooks!(
+        stage::stage_base_pre_setup,
+        stage::is_flat_stage,
+        settings::set_stage_random_settings,
+    );
+
+    stage::patch_create_stage_jump_table();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,5 @@ mod offsets;
 
 #[skyline::main(name = "stage_config")]
 fn main() {
-    skyline::install_hooks!(
-        hooks::stage::stage_base_pre_setup,
-        hooks::stage::is_flat_stage,
-        hooks::settings::set_stage_random_settings,
-    );
-
-    hooks::stage::patch_create_stage_jump_table();
+    hooks::install();
 }
