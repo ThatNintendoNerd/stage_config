1e8bc6de3433581624f7fac54e8e345007d2f70f
ThatNintendoNerd <samtheman1026@gmail.com>
2025-09-12 20:35:11 -0700

    Add panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,21 @@ fn main() {
         return;
     }
 
+    std::panic::set_hook(Box::new(|info| {
+        let location = info.location().unwrap();
+        let payload = match info.payload().downcast_ref::<&str>() {
+            Some(s) => *s,
+            None => match info.payload().downcast_ref::<String>() {
+                Some(s) => s.as_str(),
+                None => "Box<Any>",
+            },
+        };
+        let message = "stage_config has panicked! Please select Details and create an issue at https://github.com/ThatNintendoNerd/stage_config";
+        let details = format!("thread panicked at {location}:\n{payload}");
+
+        skyline::error::show_error(69, message, details.as_str());
+    }));
+
     hooks::install();
     mem::write();
 }
